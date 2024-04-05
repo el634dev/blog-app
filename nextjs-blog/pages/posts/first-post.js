2e1f5521ce5e551aaff5e7d2f598c46553fb848e
el634dev <al7758066@gmail.com>
2024-04-05 14:43:17 -0400

    Added some styling

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,18 @@
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
+// components/layout
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-    return (
-        <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-        </>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
 }
